5750ad9366f953d74ce44991b2b5d3222470dc9f
danny <dannydospuntosv@gmail.com>
2025-04-13 20:07:51 -0500

    update dependencies and add auto-import for icons, re configure vite.config.ts, update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,27 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        { path: "/", name: "home", component: () => import("@/layout.vue") },
-        { path: "/register", name: "register", component: () => import("../views/AboutView.vue") },
-        { path: "/inscriptions", name: "inscriptions", component: () => import("../views/AboutView.vue") },
-        { path: "/users", name: "users", component: () => import("../views/AboutView.vue") },
-        { path: "/assistance", name: "assistance", component: () => import("../views/AboutView.vue") }
+        {
+            path: "/", name: "home", component: () => import("@/layout.vue"),
+            children: [
+                {
+                    path: "/register", name: "newRegister", component: () => import("@/modules/registerMembers.vue"),
+                    meta: { label: "Nueva Inscripción", icon: IconSolarClipboardAddBold }
+                },
+                {
+                    path: "/inscriptions", name: "inscriptions", component: () => import("../views/AboutView.vue"),
+                    meta: { label: "Inscripciones", icon: IconTablerClipboardDataFilled }
+                },
+                {
+                    path: "/users", name: "users", component: () => import("../views/AboutView.vue"),
+                    meta: { label: "Usuarios", icon: IconSolarUsersGroupRoundedBold }
+                },
+                {
+                    path: "/assistance", name: "assistance", component: () => import("../views/AboutView.vue"),
+                    meta: { label: "Asistencia", icon: IconMaterialSymbolsCalendarAppsScript }
+                }
+            ]
+        }
     ]
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,7 +19,11 @@ export default defineConfig({
                 PrimeVueResolver()
             ]
         }),
-        Icons({ compiler: "vue3", autoInstall: true })
+        Icons({ compiler: "vue3", autoInstall: true }),
+        AutoImport({
+            resolvers: [ IconsResolver({ prefix: "Icon" }) ],
+            dts: true
+        })
     ],
     server: {
         host: true,
